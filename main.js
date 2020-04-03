1de6fbdbb2894f55656235463559b1221b110e8b
Olly Robinson <ollythedeveloper@gmail.com>
2020-04-03 12:45:52 +0100

    finish project

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const h1 = document.querySelector('.h1')
+let pixels = window.pageYOffset
+
+function updateFont(pixels, tag) {
+	const WDTH = (pixels * 0.3) + 100
+	const WGHT = (pixels * 0.6) + 100
+	tag.style.fontVariationSettings = `'wght' ${WGHT}, 'wdth' ${WDTH}`
+}
+
+window.addEventListener('scroll', () => {
+	pixels = window.pageYOffset
+	updateFont(pixels, h1)
+})
+
+updateFont(pixels, h1)
